f78be9235f17ed78d6e850d5b3251a8005684661
chamikab <kasun.chamika@gmail.com>
2019-01-15 23:46:12 +0530

    Added initial changes for setting up redux store.

--- advanced-concepts/src/index.js
+++ advanced-concepts/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore();
+
+const app = (
+  <Provider>
+    <App/>
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
