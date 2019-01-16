51050d8256d761cddb6f08839556431516b3f040
chamikab <kasun.chamika@gmail.com>
2019-01-16 23:24:42 +0530

    Added changes for leaning redux concepts.

--- advanced-concepts/src/index.js
+++ advanced-concepts/src/index.js
@@ -9,12 +9,12 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore();
 
 const app = (
-  <Provider>
+  <Provider store={store}>
     <App/>
   </Provider>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
